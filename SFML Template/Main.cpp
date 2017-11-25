422396f6ac60c054be4f78ff575528810504fef1
Kevin Stubblefield <kevins12.92@gmail.com>
2017-11-24 18:15:32 -0800

    Initial commit

---
+++ SFML Template/Main.cpp
@@ -0,0 +1,22 @@
+#include <SFML\Graphics.hpp>
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Template");
+	sf::Clock clock;
+
+	while (window.isOpen()) {
+		float deltaTime = clock.restart().asSeconds();
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+}
